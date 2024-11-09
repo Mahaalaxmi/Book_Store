8d0e35dd8364e325afc82a342126700eeba65d46
Mahalakshmi <mahakgm0827@gmail.com>
2024-11-09 22:58:12 +0530

    book functions

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -47,4 +47,44 @@ router.put("/update-book", authenticateToken, async (req, res) => {
     return res.status(500).json({ message: "An error occured" });
   }
 });
+
+router.delete("/delete-book", authenticateToken, async (req, res) => {
+  try {
+    const { bookid } = req.headers;
+    await Book.findByIdAndDelete(bookid);
+    return res.status(200).json({ message: "Book deleted successfully!" });
+  } catch (error) {
+    return res.status(500).json({ message: "An error occured" });
+  }
+});
+
+router.get("/get-all-books", async (req, res) => {
+  try {
+    const books = await Book.find().sort({ createdAt: -1 });
+    return res.json({ status: "success", data: books });
+  } catch (error) {
+    return res.status(500).json({ messgae: "An error occured" });
+  }
+});
+
+router.get("/get-recent-books", async (req, res) => {
+  try {
+    const books = await Book.find().sort({ createdAt: -1 }).limit(4);
+    return res.json({ status: "success", data: books });
+  } catch (error) {
+    return res.status(500).json({ messgae: "An error occured" });
+  }
+});
+
+router.get("/get-book-by-id/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findById(id);
+    return res.json({ status: "success", data: book });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ messgae: "An error occured" });
+  }
+});
+
 module.exports = router;
