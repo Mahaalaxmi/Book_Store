3852efc927a293ab284be38606798616505047ca
Mahalakshmi <mahakgm0827@gmail.com>
2024-11-10 23:22:05 +0530

    favouriteBooks added

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,18 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("./config/db");
-const user = require("./routes/user");
-const books = require("./routes/book");
+const User = require("./routes/user");
+const Books = require("./routes/book");
+const Favourite = require("./routes/favourite");
 // app.get("/", (req, res) => {
 //   res.send("Hello From backend site");
 // });;
 
 app.use(cors());
 app.use(express.json());
-app.use("/users", user);
-app.use("/books", books);
+app.use("/users", User);
+app.use("/books", Books);
+app.use("/fav", Favourite);
 
 app.get("/", (req, res) => {
   res.status(200).json("Welcome");
