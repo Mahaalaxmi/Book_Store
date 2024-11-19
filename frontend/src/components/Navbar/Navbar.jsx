0f3e20349fc77bde11a30105423ad00c8d36c7e0
Mahalakshmi <mahakgm0827@gmail.com>
2024-11-19 22:00:05 +0530

    navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { TiThMenu } from "react-icons/ti";
 
 const Navbar = () => {
   const links = [
@@ -26,7 +27,7 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="flex bg-zinc-800 text-white px-8 py-4 items-center justify-between">
+      <nav className="z-50 relative flex bg-zinc-800 text-white px-8 py-4 items-center justify-between">
         <Link to="/" className="flex items-center">
           <img
             src="https://cdn-icons-png.flaticon.com/128/10433/10433049.png"
@@ -35,8 +36,8 @@ const Navbar = () => {
           />
           <h1 className="text-2xl font-semibold">BookHeaven</h1>
         </Link>
-        <div className="nav-links-bookheaven flex items-center gap-4">
-          <div className="flex gap-4">
+        <div className="nav-links-bookheaven block md:flex items-center gap-4">
+          <div className="hidden md:flex gap-4">
             {links.map((items, i) => (
               <Link
                 to={items.link}
@@ -47,7 +48,7 @@ const Navbar = () => {
               </Link>
             ))}
           </div>
-          <div className="flex gap-4 ">
+          <div className="hidden md:flex gap-4 ">
             <Link
               to="/Login"
               className="px-4 py-1 border border-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300"
@@ -61,7 +62,21 @@ const Navbar = () => {
               SignUp
             </Link>
           </div>
+          <button className="text-white text-2xl hover:text-zinc-400">
+            <TiThMenu />
+          </button>
         </div>
+      </nav>
+      <div className="bg-zinc-800 h-screen absolute top-0 left-0 w-full z-40 flex flex-col items-center justify-center">
+        {links.map((items, i) => (
+          <Link
+            to={items.link}
+            key={i}
+            className="text-white text-4xl mb-8 font-semibold hover:text-blue-500 transition-all duration-300"
+          >
+            {items.title}
+          </Link>
+        ))}
       </div>
     </>
   );
