0f910b9b10ec01e5a33ed5ace853b75b39b289e1
Mahalakshmi <mahakgm0827@gmail.com>
2024-11-20 23:04:18 +0530

    commit

--- frontend/src/pages/AllBooks.jsx
+++ frontend/src/pages/AllBooks.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const AllBooks = () => {
-  return <div>AllBooks</div>;
+  return (
+    <>
+      <h1>AllBooks</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
+        recusandae error vitae id maxime laboriosam ut asperiores ducimus
+        reiciendis possimus veritatis alias cumque autem, dolorem necessitatibus
+        neque perspiciatis molestias consequuntur.
+      </p>
+    </>
+  );
 };
 
 export default AllBooks;
